3fd1d5cf965adf6cb3b241b18c80709ac2142b91
Marcin Zdun <mzdun@midnightbits.com>
2022-12-30 15:04:30 +0100

    ci: fix clean install issues

--- libs/websocket/appstr.cmake
+++ libs/websocket/appstr.cmake
@@ -3,6 +3,11 @@ set(MUSTACHE_DIR "${DATA_DIR}/templates")
 set(LNGS_FILE "${DATA_DIR}/strings/webapp.lngs")
 
 function(add_idl_mustache OUTPUT MUSTACHE)
+    get_filename_component(_DIR "${OUTPUT}" DIRECTORY)
+    add_custom_command(OUTPUT "${_DIR}"
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${_DIR}"
+        VERBATIM)
+
     add_custom_command(OUTPUT "${OUTPUT}"
         COMMAND mbits::lngs
         ARGS mustache
@@ -11,7 +16,7 @@ function(add_idl_mustache OUTPUT MUSTACHE)
             --tmplt-dir "${MUSTACHE_DIR}"
             --tmplt ${MUSTACHE}
         MAIN_DEPENDENCY "${LNGS_FILE}"
-        DEPENDS "${MUSTACHE_DIR}/${MUSTACHE}.mustache"
+        DEPENDS "${MUSTACHE_DIR}/${MUSTACHE}.mustache" "${_DIR}"
         VERBATIM)
     message(STATUS "{{> ${MUSTACHE}}} ${OUTPUT}")
 endfunction()
