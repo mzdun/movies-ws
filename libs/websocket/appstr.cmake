6614016cc52b60820aeb0b88343c7cbee088d315
Marcin Zdun <mzdun@midnightbits.com>
2023-01-03 14:49:35 +0100

    build: VS-gen doesn't like dir targets

--- libs/websocket/appstr.cmake
+++ libs/websocket/appstr.cmake
@@ -4,11 +4,8 @@ set(LNGS_FILE "${DATA_DIR}/strings/webapp.lngs")
 
 function(add_idl_mustache OUTPUT MUSTACHE)
     get_filename_component(_DIR "${OUTPUT}" DIRECTORY)
-    add_custom_command(OUTPUT "${_DIR}"
-        COMMAND ${CMAKE_COMMAND} -E make_directory "${_DIR}"
-        VERBATIM)
-
     add_custom_command(OUTPUT "${OUTPUT}"
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${_DIR}"
         COMMAND mbits::lngs
         ARGS mustache
             "${LNGS_FILE}"
@@ -16,7 +13,7 @@ function(add_idl_mustache OUTPUT MUSTACHE)
             --tmplt-dir "${MUSTACHE_DIR}"
             --tmplt ${MUSTACHE}
         MAIN_DEPENDENCY "${LNGS_FILE}"
-        DEPENDS "${MUSTACHE_DIR}/${MUSTACHE}.mustache" "${_DIR}"
+        DEPENDS "${MUSTACHE_DIR}/${MUSTACHE}.mustache"
         VERBATIM)
     message(STATUS "{{> ${MUSTACHE}}} ${OUTPUT}")
 endfunction()
